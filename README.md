bca73a55a54abf1c4ba3f7255114c79ea9176070
Diego Ardila <ardila.d.ardila@gmail.com>
2016-11-10 09:16:43 -0800

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # paintingReorganize
+
+## To install:
+
+pip install -r requirements.txt
+
+If that doesn't work you will have to manually track down and install these pip packages.
+
+## To use
+
+python palette.py /path/to/input_file.jpg
+
+Output will be written to output.jpg
---
+++ palette.py
@@ -0,0 +1,53 @@
+from PIL import Image
+
+import scipy
+import numpy as np
+import os
+import sys
+from sklearn.decomposition import PCA
+
+filename = os.path.expanduser(sys.argv[1]) 
+
+image = scipy.misc.imread(filename)[:,:, :3]
+
+original_shape = image.shape
+
+pixels = image.reshape((original_shape[0]*original_shape[1], 3))
+
+pca = PCA(n_components = 1)
+
+first_pca_component = np.squeeze(pca.fit_transform(pixels))
+
+pixels_by_first_component = pixels[np.argsort(first_pca_component)]
+
+print pixels_by_first_component.shape
+
+n_rows = original_shape[0]
+n_columns = original_shape[1]
+
+new_image = np.zeros_like(image)
+
+old_column = None
+
+for column in range(n_columns):
+  pixels_in_column = pixels_by_first_component[n_rows*column:(column+1)*n_rows]
+  print pixels_in_column.shape
+  first_column_component = np.squeeze(pca.fit_transform(pixels_in_column))
+  new_column = pixels_in_column[np.argsort(first_column_component)]
+  
+  if old_column is not None:
+    distance = np.mean(np.sqrt(np.sum((new_column - old_column)**2, 1)))
+    flipped_distance = np.mean(np.sqrt(np.sum((new_column[-1::-1] - old_column)**2, 1)))
+    print distance-flipped_distance
+    if flipped_distance < distance:
+      new_column = new_column[-1::-1]   
+  old_column = new_column
+
+  new_image[:, column, :] = new_column
+
+
+im = Image.fromarray(new_image)
+im.save("output.png")
+  
+  
+  
